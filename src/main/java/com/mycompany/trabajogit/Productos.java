e79488f83facf17dcdbb35a4efee5dc51dd179b7
camiloac98 <camiloalbertoc98@gmail.com>
2022-08-18 20:40:46 -0500

    Version 2 Clase producto creada

---
+++ src/main/java/com/mycompany/trabajogit/Productos.java
@@ -0,0 +1,15 @@
+package com.mycompany.trabajogit;
+
+public class Productos {
+    private String nombre;
+    private double precio;
+    private int cantidad;
+
+    public Productos(String nombre, double precio, int cantidad) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+    
+    
+}
